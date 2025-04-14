9e78a3b95be2eca88b527985ab8abc500358d531
Seung-min Yu <67177134+smyu24@users.noreply.github.com>
2025-04-14 16:34:59 -0700

    Update model.js

--- model.js
+++ model.js
@@ -1,61 +1,33 @@
-  // Classifier Variable
-  let classifier;
-  // Model URL
-  let imageModelURL = 'https://teachablemachine.withgoogle.com/models/jeD3mSA2D/';
-  
-  // Video
-  let video;
-  let flippedVideo;
-  // To store the classification
-  let label = "";
+let classifier;
+let label = "listening";
 
-  // Load the model first
-  function preload() {
-    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+// Use the base URL, not the model.json file
+let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/';
+
+function preload() {
+  classifier = ml5.soundClassifier(soundModelURL + 'model.json', modelReady);
+}
+
+function setup() {
+  createCanvas(320, 240);
+}
+
+function modelReady() {
+  classifier.classify(gotResult);
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text(label, width / 2, height / 2);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+    return;
   }
-
-  function setup() {
-    createCanvas(320, 260);
-    // Create the video
-    video = createCapture(VIDEO);
-    video.size(320, 240);
-    video.hide();
-
-    flippedVideo = ml5.flipImage(video);
-    // Start classifying
-    classifyVideo();
-  }
-
-  function draw() {
-    background(0);
-    // Draw the video
-    image(flippedVideo, 0, 0);
-
-    // Draw the label
-    fill(255);
-    textSize(16);
-    textAlign(CENTER);
-    text(label, width / 2, height - 4);
-  }
-
-  // Get a prediction for the current video frame
-  function classifyVideo() {
-    flippedVideo = ml5.flipImage(video)
-    classifier.classify(flippedVideo, gotResult);
-    flippedVideo.remove();
-
-  }
-
-  // When we get a result
-  function gotResult(error, results) {
-    // If there is an error
-    if (error) {
-      console.error(error);
-      return;
-    }
-    // The results are in an array ordered by confidence.
-    // console.log(results[0]);
-    label = results[0].label;
-    // Classifiy again!
-    classifyVideo();
-  }
+  label = results[0].label;
+}
