405d1c1458344d8f698ca4ea23b4fc68bd8ec3cb
Seung-min Yu <67177134+smyu24@users.noreply.github.com>
2025-04-14 16:43:17 -0700

    Update model.js

--- model.js
+++ model.js
@@ -1,33 +1,41 @@
 let classifier;
-let label = "listening";
 
-// Use the base URL, not the model.json file
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/';
+// Label
+let label = 'listening...';
+
+// Teachable Machine model URL:
+let soundModel = 'https://teachablemachine.withgoogle.com/models/vDNbZ994M/';
+
 
 function preload() {
-  classifier = ml5.soundClassifier(soundModelURL + 'model.json', modelReady);
+  // Load the model
+  classifier = ml5.soundClassifier(soundModel + 'model.json');
 }
 
 function setup() {
   createCanvas(320, 240);
-}
-
-function modelReady() {
+  // Start classifying
+  // The sound model will continuously listen to the microphone
   classifier.classify(gotResult);
 }
 
 function draw() {
   background(0);
+  // Draw the label in the canvas
   fill(255);
   textSize(32);
   textAlign(CENTER, CENTER);
   text(label, width / 2, height / 2);
 }
 
+
+// The model recognizing a sound will trigger this event
 function gotResult(error, results) {
   if (error) {
     console.error(error);
     return;
   }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
   label = results[0].label;
 }
